2c0f6db9f8e504665d2524611222e698e61c7db9
Nazmul Hossain <130008966+NazmulNSince1998@users.noreply.github.com>
2023-04-07 17:32:42 +0600

    UpApp.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 window.onload = () => {
-    //console.log('This is your first program executing!');
+    console.log('This is your first program executing!');
 }
+console.log("This is my first Javascript online coding as well as using GitHub and things like commit pull push etc.")
